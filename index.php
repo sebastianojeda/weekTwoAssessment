04fe5ba14a7364d73851e74a60658d3eba0a1335
sebastianojeda <sebastianojeda.school@gmail.com>
2015-04-29 11:52:27 -0700

    initial commit done with quiz button one sends an alert and button two fades when click on

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+</head>
+<body>
+	<div class="dan">
+		<button>
+			click me
+		</button>
+	</div>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<div class="iwao">
+		<button>
+			click me
+		</button>
+	</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Event setup using a convenience method
+$( ".dan" ).click(function() {
+    alert( "Hey you clicked on me" );
+});
+
+$( ".iwao" ).click(function() {
+    $(".iwao").hide("fast");
+});
